41bf4d14c11a6675e84be66139468c2355036c71
Leela0o5 <leela592023@gmail.com>
2026-01-08 16:20:46 +0530

    Changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const BASE_URL = process.env.REACT_APP_API_URI || "http://localhost:5000/api";
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
